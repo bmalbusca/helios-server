df63a5c7455d9b723f24db7dfa4573b1c841ebf1
Debian-cloud-init-user <debian@debian.athens-dev.al.vps.tecnico.ulisboa.pt>
2021-03-09 14:45:32 +0000

    Add supervisor celery_beat config

--- deploy/supervisor/supervisor-service.sh
+++ deploy/supervisor/supervisor-service.sh
@@ -3,9 +3,12 @@ if [[ `uname` == 'Linux' ]]; then
 d="/var/log/celery"
 cp /var/www/helios-server/deploy/supervisor/celery-worker.conf /etc/supervisor/conf.d/
 [ -d "${d}" ] &&  echo "Directory $d found." ||  mkdir /var/log/celery/
-sudo chmod 766 /var/log/celery/
+sudo chmod 777 /var/log/celery/
+sudo chmod 777 /var/www/helios-server/.env
 sudo service supervisor start
 supervisorctl reread
 supervisorctl update
-sudo supervisorctl start celery-worker
+sudo supervisorctl start athens:*
+sudo supervisorctl restart athens:*
+sudo supervisorctl status
 fi
